e204ecb3f52f6b6f34f50dfe8569fdd489cc0c79
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-04 22:23:59 +0100

    ex2 check

--- week1/exercise2.js
+++ week1/exercise2.js
@@ -6,10 +6,13 @@ Develop a small JS program to manage the list of users in a Q&A website.
 
     Define the names of users as a comma-separated list.
     For instance: "Luigi De Russis, Luca Mannella, Fulvio Corno, Juan Pablo Saenz Moreno, Enrico Masala, Antonio Servetti, Eros Fani"
+    
     Parse the string and create an array containing one name per array position.
     Beware: no extra spaces should be present.
+    
     Create a second array by computing the acronyms of the people as the initial letters of the name. Acronyms should be in all-capital letters.
     For example, Luigi De Russis -> LDR.
+    
     Print the resulting list of acronyms and the full names.
     Extra: in alphabetical order of acronym.
  */
@@ -27,5 +30,5 @@ const regex = /[^A-Z]/g; // need gloabl flag for replaceAll
 let users_acro = users_array.map((name) => name.replaceAll(regex, ""));
 
 // sort and print
-console.log(users_array.sort().toString());
-console.log(users_acro.sort().toString());
+console.log("\n" + users_array.sort().toString());
+console.log("\n" + users_acro.sort().toString());
