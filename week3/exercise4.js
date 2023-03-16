f88a971496289bca11673e34ac7a27506c81ef5a
Gabriele Quaranta <quaranta.gabriele99@gmail.com>
2023-03-16 15:06:27 +0100

    exercise4

--- week3/exercise4.js
+++ week3/exercise4.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const dayjs = require("dayjs");
+const { result } = require("lodash");
 const sqlite3 = require("sqlite3");
+
+const dateformat = "YYYY/MM/DD";
 
 // asnwers class
 class AnswerClass {
@@ -25,7 +28,7 @@ class QuestionClass {
   async add(ans) {
     await this.db.run(
       "insert into answers(text,respondent,score,date) values (?,?,?,?)",
-      [ans.response, ans.name, ans.score, ans.date.format("DD/MM/YYYY")],
+      [ans.response, ans.name, ans.score, ans.date.format(dateformat)],
       (res, err) => {
         if (err) console.log(res + " ERR_INS");
       }
@@ -33,11 +36,51 @@ class QuestionClass {
   }
 
   async getAll() {
-    await this.db.all("select * from answers", [], (err, rows) => {
-      if (err) throw err;
-      return rows;
+    let query = "select * from answers";
+
+    return new Promise((resolve, reject) => {
+      this.db.all(query, [], (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      });
     });
   }
+
+  async find(respondent) {
+    let query = "select * from answers where respondent=?";
+    return new Promise((resolve, reject) => {
+      this.db.all(query, respondent, (err, rows) => {
+        if (err) reject(err);
+        if (rows) resolve(rows);
+      });
+    });
+  }
+
+  async afterDate(date) {
+    let query = "select * from answers where date<?";
+    return new Promise((resolve, reject) => {
+      this.db.all(query, date, (err, rows) => {
+        if (err) reject(err);
+        if (rows) resolve(rows);
+      });
+    });
+  }
+
+  async getWorst() {
+    let query = "select * from answers order by score asc";
+    return new Promise((resolve, reject) => {
+      this.db.get(query, (err, rows) => {
+        if (err) reject(err);
+        if (rows) resolve(rows);
+      });
+    });
+  }
+}
+
+function sep() {
+  console.log();
 }
 
 // MAIN
@@ -50,23 +93,42 @@ async function main() {
   let question = new QuestionClass(
     "what day is it",
     "gab",
-    dayjs("15-03-2022").format("YYYY/MM/DD"),
+    dayjs("15-03-2022").format(dateformat),
     db
   );
 
-  let a1 = new AnswerClass("tues", "gab", 3, dayjs("2022-03-18"));
-  let a2 = new AnswerClass("mon", "gab", 0, dayjs("2019-03-06"));
-  let a3 = new AnswerClass("wed", "gab", 6, dayjs("2023-06-08"));
-
+  //reset db file
   await question.db.run("delete from answers where id>0", (res, err) => {
     if (err) console.log(res + "ERR_DEL");
   });
+  await question.db.run(
+    "update sqlite_sequence set seq=0 where name=answers",
+    (res, err) => {
+      if (err) console.log(res + "ERR_DEL");
+    }
+  );
+
+  let a1 = new AnswerClass("tues", "gab", 3, dayjs("2022-03-18"));
+  let a2 = new AnswerClass("mon", "matt", 0, dayjs("2019-03-06"));
+  let a3 = new AnswerClass("wed", "sss", 6, dayjs("2023-06-08"));
 
   await question.add(a1);
   await question.add(a2);
   await question.add(a3);
 
-  console.log(await question.getAll().toString()); //RETURN promise NEED check resolution
+  sep();
+  await question.getAll().then((r) => console.log(r));
+
+  sep();
+  let respondent = "gab";
+  await question.find(respondent).then((r) => console.log(r));
+
+  sep();
+  let qdate = dayjs("2022-03-18").format(dateformat);
+  await question.afterDate(qdate).then((r) => console.log(r));
+
+  sep();
+  await question.getWorst().then((r) => console.log(r));
 }
 
 main();
